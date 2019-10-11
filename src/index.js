636a429b8aab48f8ca2ff70830459319dab298a8
Anson Cheung <ansoncwork@gmail.com>
2019-10-11 16:38:29 +0100

    Update structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Array
+const { cusum } = require("./utils/array/cusum");
+
+// Image
+const { saveDivAsImage } = require("./utils/image/saveDivAsImage");
+
+module.exports = {
+  cusum,
+  saveDivAsImage
+};
---
+++ src/utils/array/cusum.test.js
@@ -0,0 +1,16 @@
+const { cusum } = require("./cusum");
+
+test("cusum of [1,2] should be [1,3]", () => {
+  var result = cusum([1, 2]);
+  expect(result).toStrictEqual([1, 3]);
+});
+
+test("cusum of [1,2,3,4,5] should be [1,3,6,10,15]", () => {
+  var result = cusum([1, 2, 3, 4, 5]);
+  expect(result).toStrictEqual([1, 3, 6, 10, 15]);
+});
+
+test("cusum of [2,4,6,8,10] should be [2,6,12,20,30]", () => {
+  var result = cusum([2, 4, 6, 8, 10]);
+  expect(result).toStrictEqual([2, 6, 12, 20, 30]);
+});
